79a09f059b448b125e90a3076601abae389ee35c
Andrey Galaktionov <das.frama@gmail.com>
2020-07-10 17:48:14 +0300

    Добавил конфиг.

---
+++ pkg/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+// Config main app configuration struct.
+type Config struct {
+	DB struct {
+		Path       string
+		Migrations string
+	}
+}
+
+// LoadConfig creates a struct from file.
+func LoadConfig(filepath string) (Config, error) {
+	var config Config
+	// Open file.
+	file, err := os.Open("config.json")
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	// Parse config.
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
