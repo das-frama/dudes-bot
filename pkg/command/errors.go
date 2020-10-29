0746a92971421e0c5f35033ed1ca0824592b90a3
Andrey Galaktionov <das.frama@gmail.com>
2020-10-29 14:41:03 +0300

    Обновил описание ошибки.

--- pkg/command/errors.go
+++ pkg/command/errors.go
@@ -3,4 +3,4 @@ package command
 import "errors"
 
 // ErrWrongDateFormat is used when file doesn't have an .md extension.
-var ErrWrongDateFormat = errors.New("ты, блять, сначала научись правильно вводить даты, а уже потом обращайся ко мне")
+var ErrWrongDateFormat = errors.New("ой простити-извините, вы не совсем верно написали дату. Ой, пожалуйста, укажите правильный формат dd.mm.yyyy или dd.mm")
